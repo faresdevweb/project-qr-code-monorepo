9f901b87234ef01941fd423a4798f106ccc54ed5
faresdevweb <fares.meier@hotmail.com>
2023-10-31 11:33:46 +0100

    feat: added create maintenance account route

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { signInDTO } from './dto';
+import { signInDTO, maintenanceAccountDTO } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +11,10 @@ export class AuthController {
     @Post('signin')
     signin(@Body() signInDTO: signInDTO){
         return this.authService.signin(signInDTO);
+    }
+
+    @Post('maintenance')
+    createMaintenanceAccount(@Body() maintenanceAccountDTO: maintenanceAccountDTO){
+        return this.authService.createMaintenanceAccount(maintenanceAccountDTO)
     }
 }
--- packages/backend/src/auth/dto/index.ts
+++ packages/backend/src/auth/dto/index.ts
@@ -1,1 +1,2 @@
 export * from './signIn.dto';
+export * from './maintenanceAccount.dto';
