494faeec1a451b3dbcc1e2835bd114bfa303221d
faresdevweb <fares.meier@hotmail.com>
2023-10-31 11:26:56 +0100

    config: config main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(4000, '192.168.1.25');
 }
 bootstrap();
