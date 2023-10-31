000dd11e7a91952fcb973830b5ecf7a9f0f95ed4
faresdevweb <fares.meier@hotmail.com>
2023-10-31 13:05:46 +0100

    feat(auth): added route verifyToken

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -40,8 +40,6 @@ export class AuthService {
             email: user.email
         };
     
-        
-
         if (['ADMIN', 'TEACHER', 'STUDENT'].includes(user.role)) {
             
             if (!user.schoolId) {
