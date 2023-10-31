1744d05ab0b8e0fabb3e4b88be86fd3d06e38a4b
faresdevweb <fares.meier@hotmail.com>
2023-10-31 10:43:12 +0100

    config: added prisma module and prisma schema

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
