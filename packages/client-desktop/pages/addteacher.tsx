06f1c6a22f2ad64d5d7e7f5bfa8bed01c92bb530
faresdevweb <fares.meier@hotmail.com>
2023-11-01 21:00:23 +0100

    feat(pages): added add teacher page

---
+++ packages/client-desktop/pages/addteacher.tsx
@@ -0,0 +1,57 @@
+import Layout from '@/components/Layout';
+import { useAuth } from '@/hooks/useAuth';
+import { useForm } from 'react-hook-form';
+import { useCookies } from 'react-cookie';
+import { useRegister } from '@/hooks/useRegister';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+import jwt from 'jsonwebtoken';
+import { DecodedToken } from '@/interfaces/DecodedToken.interface';
+
+const AddTeacher = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { registerTeacher } = useRegister();
+
+  const [cookies] = useCookies(['token']);
+  const decodedToken: DecodedToken | string | any = jwt.decode(
+    cookies.token,
+  );
+  const { isAuthenticated } = useAuth();
+
+  const onSubmit = (data: any) => {
+    const formData = new FormData();
+    formData.append('csv', data.csv[0]);
+    formData.append('schoolId', decodedToken.schoolCustomId);
+    registerTeacher(formData, cookies.token);
+  };
+
+  if (!isAuthenticated)
+    return <div className="text-center text-xl">Loading...</div>;
+
+  return (
+    <Layout userRole={decodedToken.role}>
+      <div className="p-5 h-screen">
+        <h1 className="mb-2">Register teacher</h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="border border-black flex flex-col gap-4 w-[500px] p-3"
+        >
+          <Input
+            variant="file"
+            label="Import Students"
+            placeholder="First name"
+            {...register('csv')}
+            className="w-[300px]"
+          />
+          <Button variant="primary" label="Submit" type="submit" />
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default AddTeacher;
