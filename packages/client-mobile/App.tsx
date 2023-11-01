813760963887dbad1c4b5cc3016fc61b18c93917
faresdevweb <fares.meier@hotmail.com>
2023-11-01 22:04:11 +0100

    fix: fix expo start and app.tsx in client-mobile

--- packages/client-mobile/App.tsx
+++ packages/client-mobile/App.tsx
@@ -1,12 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthStore } from './store/auth';
+import AppTabs from './src/components/AppTabs';
+import AuthTabs from './src/components/AuthTabs';
 
 export default function App() {
+  const { isAuthenticated } = AuthStore();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {isAuthenticated ? <AppTabs /> : <AuthTabs />}
+    </NavigationContainer>
   );
 }
 
